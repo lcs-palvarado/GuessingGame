9c67c8b4afd235accc052393a51c9c3c5dd6a498
Pablo Alvarado <palvarado@lcs.on.ca>
2024-02-02 09:47:40 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Pablo Alvarado on 02/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
